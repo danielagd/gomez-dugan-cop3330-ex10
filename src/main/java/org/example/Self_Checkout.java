0bc137a77eea8e23a6e06727448d1268ce26d552
Daniela <danigomezdugan@gmail.com>
2021-09-09 21:51:42 -0400

    updating file

--- src/main/java/org/example/Self_Checkout.java
+++ src/main/java/org/example/Self_Checkout.java
@@ -9,11 +9,14 @@ public class Self_Checkout
 {
     public static void main( String[] args )
     {
+        // Program to calculate total of three items in a supermarket
+
         final double tax_rate = 0.055;
         Scanner input = new Scanner(System.in);
 
         double subtotal = 0.0;
 
+        // Ask the user for information of each of the three items
         for (int i = 0; i < 3; i++)
         {
             System.out.print("Enter the price of item " + (i+1) + ": ");
@@ -25,6 +28,7 @@ public class Self_Checkout
 
         System.out.println("Subtotal: $" + String.format("%.2f", subtotal));
 
+        // Calculate tax
         double tax = subtotal * tax_rate;
         System.out.println("Tax: $" + String.format("%.2f", tax));
 
